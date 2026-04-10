163d04dbdd178dfc09776593c93a235445ac4850
BttCrtd <courtadon.baptiste@gmail.com>
2026-04-10 10:06:27 +0200

    Intégration Filtres

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,22 @@ import Image from "next/image";
 import styles from "@/styles/page.module.css";
 import recipes from "../../data/recipes.json";
 import { RecipeCard } from "@/components/RecipeCard/RecipeCard";
+import { Filter } from "@/components/Filter/Filter";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <div className={styles.filters_container}>
+          <div className={styles.filters}>
+            <Filter category="Ingrédients" />
+            <Filter category="Appareils" />
+            <Filter category="Ustensiles" />
+          </div>
+          <div className={styles.counter_recipes}>
+            <p>50 recettes</p>
+          </div>
+        </div>
          <div className={styles.recipes_container}>
           {recipes.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe} />
