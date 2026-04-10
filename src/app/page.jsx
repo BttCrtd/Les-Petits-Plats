fa98c1ea703cf7e136750a9bc6074d2ce25ae409
BttCrtd <courtadon.baptiste@gmail.com>
2026-04-10 09:59:30 +0200

    Intégration des cartes recette

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import styles from "@/styles/page.module.css";
+import recipes from "../../data/recipes.json";
+import { RecipeCard } from "@/components/RecipeCard/RecipeCard";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+         <div className={styles.recipes_container}>
+          {recipes.map((recipe) => (
+            <RecipeCard key={recipe.id} recipe={recipe} />
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import "@/styles/recipeCard.css";
+
+export function RecipeCard({ recipe }) {
+  return (
+    <Link href={`/recipe/${recipe.id}`} className="recipe-card-container">
+      <article>
+        <div className="time">
+          <p>{recipe.time}min</p>
+        </div>
+        <Image
+          src={`/images/${recipe.image}`}
+          alt={`Photo ${recipe.name}`}
+          width={380}
+          height={253}
+          className="recipe-image"
+        />
+        <div className="card-contents">
+          <h2>{recipe.name}</h2>
+          <h3>Recette</h3>
+          <p>{recipe.description}.</p>
+          <h3>Ingrédients</h3>
+          <div className="list-ingredients">
+            <ul>
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>
+                  <div className="ingredien">
+                    <span>{ingredient.ingredient}</span>
+                    <p>
+                      {ingredient.quantity ? ingredient.quantity : "-"}
+                      {ingredient.unit ? ` ${ingredient.unit}` : ""}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </article>
+    </Link>
+  );
+}
